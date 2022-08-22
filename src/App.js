625ddf8b1c10e32dc7667b41fbb7e6165b4e2d0f
BridgitKirika <kirikabetty@gmail.com>
2022-08-22 14:43:45 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from "./components/Navbar";
+import Home from './pages/Home';
+
+function App() {
+  return (
+    <>
+    <Router>
+      <Navbar />
+      <Routes>
+      <Route path='/' component={Home} />
+      </Routes>
+    </Router>
+    </>
+    
+  );
+}
+
+export default App;
